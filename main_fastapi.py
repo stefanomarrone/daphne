135586111ae152aa87d6dc5bedcbe9028d410d17
Stefano Marrone <stefano.marrone@unicampania.it>
2024-05-30 09:25:56 +0200

    Refactoring

--- main_fastapi.py
+++ main_fastapi.py
@@ -1,26 +1,28 @@
-import json
-import os
 from typing import List
-
 from fastapi import FastAPI, requests
 import requests
 from pydantic import BaseModel
 import uvicorn
 
-app = FastAPI()
-
 # Definisci il modello dei dati JSON
 class Item(BaseModel):
     name: str
     content: str
+
+
 class Configurations(BaseModel):
     configurations: List[Item]
 
+app = FastAPI()
 
 #Endpoint che legge Json
-@app.post("/items/") #router.get
-async def create_item(item: Configurations):
+@app.post("/execute/") #router.get
+def create_item(item: Configurations):
+
+
     return {"item": item}
+
+
 
 uvicorn.run(app,port=4416)
 

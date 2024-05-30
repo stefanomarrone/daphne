45dcc6332baa985e8ff5e5931d7aa56c082d53eb
Stefano Marrone <stefano.marrone@unicampania.it>
2024-05-30 09:41:29 +0200

    Splitting the grub function

--- src/core.py
+++ src/core.py
@@ -1,20 +1,15 @@
 from src.abstractfactory import EngineFactory
 from src.concretefactory import FactoryGenerator
 from src.configuration import Configuration
+import tempfile
 
 
-def grab(configurationname):
-    configuration = Configuration(configurationname)
-    # Image section
-    imageEngine = EngineFactory.generateImage(configuration)
-    imagefactory = FactoryGenerator.generate(imageEngine)
-    imagegrabber = imagefactory().generate(configuration)
-    imagegrabber.grub()
-    # Data section
-    dataEngine = EngineFactory.generateData(configuration)
-    datafactory = FactoryGenerator.generate(dataEngine)
-    datagrabber = datafactory().generate(configuration)
-    datagrabber.grub()
+def grab(configuration):
+    tmp = tempfile.NamedTemporaryFile()
+    with open(tmp.name, 'w') as f:
+        f.write(configuration)
+        f.flush()
+        grab_name(tmp.name)
 
 
 def grab_name(configurationname):

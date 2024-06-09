48ce81a119d5e83061559807c6017949aad7b72c
Stefano Marrone <stefano.marrone@unicampania.it>
2024-06-09 21:44:28 +0200

    Dataclasses on the response data are created

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from typing import List
 from pydantic import BaseModel
-
+from dataclasses import dataclass, asdict, field
 
 class Experiment(BaseModel):
     name: str
@@ -9,3 +9,34 @@ class Experiment(BaseModel):
 
 class Experiments(BaseModel):
     configurations: List[Experiment]
+
+
+@dataclass
+class FileResponse:
+    filename: str
+    result: bool
+
+    def dict(self):
+        _dict = self.__dict__.copy()
+        return _dict
+
+
+@dataclass
+class SingleResponse:
+    configurationname: str
+    response: bool
+    errormessage: str
+    files: List[FileResponse]
+
+    def dict(self):
+        _dict = self.__dict__.copy()
+        return _dict
+
+
+@dataclass
+class Response:
+    reponses: List[SingleResponse]
+
+    def dict(self):
+        _dict = self.__dict__.copy()
+        return _dict

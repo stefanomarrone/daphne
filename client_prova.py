3035528d265788149b29a1831b94097ae8fb135c
Stefano Marrone <stefano.marrone@unicampania.it>
2024-06-10 15:16:54 +0200

    simple test client

--- client_prova.py
+++ client_prova.py
@@ -1,36 +1,16 @@
 import os
+import time
 import requests
 import json
 
-mongoport = 1812
-mongoip = '127.0.0.1'
 
+time.sleep(3)
+url = "http://127.0.0.1:12345/execute"
 file_path = os.path.join('inputs', 'input.json')
-
-# with open(file_path, 'r') as file:
-#     data = json.load(file)
-#
-#
-# x = data['configurations']
-# y = x[0]['name']
-
-
-image_path = os.path.join('output', 'Untitled.jpeg')
-image_path2 = os.path.join('output2', 'Untitled.jpeg')
-
-
-def addingDDM():
-    mongourl = 'http://' + mongoip + ':' + str(mongoport) + '/matforpat?identifier=' + 'configuration.ini'
-    handler = open("./output/Untitled.jpeg", 'rb')
-    files = {"file":( handler.name, handler, "multipart/form-data")}
-    resp = requests.post(url=mongourl,files=files)
-
-    return resp.json()['success']
+with open(file_path, 'r') as file:
+    data = json.load(file)
+response = requests.post(url, json=data)
+print(response.json())
 
 
 
-if __name__ == '__main__':
-
-    print("Adding the Data Driven Model")
-    result = addingDDM()
-    print(result)

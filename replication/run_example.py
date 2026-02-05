7e09e40dffd600bc5850efadb7b80b34e35bcfbc
stella <stella.debiase@gmail.com>
2026-02-05 12:37:12 +0100

    replication example update

--- replication/run_example.py
+++ replication/run_example.py
@@ -37,7 +37,7 @@ def main():
     parser.add_argument(
         "--entrypoint",
         type=str,
-        default="main.py",
+        default="cmd_main.py",
         help="Entrypoint script (relative to project root).",
     )
     args = parser.parse_args()
